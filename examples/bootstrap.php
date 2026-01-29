43d4b20fd966b937c03bbb4a0745b51c84ab18c7
Vic Cherubini <vcherubini@gmail.com>
2026-01-29 16:13:06 -0600

    wip

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -5,6 +5,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\PropertyInfo\Extractor\ConstructorExtractor;
 use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AttributeLoader;
+use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
@@ -14,6 +17,14 @@ use Symfony\Component\Serializer\Serializer;
 
 function createSerializer(): Serializer
 {
+    $classMetadataFactory = new ClassMetadataFactory(...[
+        'loader' => new AttributeLoader(true, []),
+    ]);
+
+    $metadataAwareNameConverter = new MetadataAwareNameConverter(...[
+        'metadataFactory' => $classMetadataFactory,
+    ]);
+
     // Construct the Symfony Normalizers and Denormalizers
     $propertyInfoExtractor = new PropertyInfoExtractor([], [
         new ConstructorExtractor([new PhpDocExtractor()]),
@@ -25,8 +36,8 @@ function createSerializer(): Serializer
         new DateTimeNormalizer(),
         new UnwrappingDenormalizer(),
 
-        // This must come last so it can use the UnwrappingDenormalizer
-        new ObjectNormalizer(propertyInfoExtractor: $propertyInfoExtractor),
+        // This must come last so the denormalizer injected into the client can use the UnwrappingDenormalizer
+        new ObjectNormalizer($classMetadataFactory, $metadataAwareNameConverter, null, null, null, null, [], $propertyInfoExtractor),
     ]);
 
     return $serializer;
