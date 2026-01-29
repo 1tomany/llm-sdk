52a45251996c06f14d55cf988fe945a002784b0c
Vic Cherubini <vcherubini@gmail.com>
2026-01-29 11:00:04 -0600

    wip

---
+++ src/Request/File/CacheFileRequest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace OneToMany\AI\Request\File;
+
+use OneToMany\AI\Contract\Request\File\CacheFileRequestInterface;
+use OneToMany\AI\Exception\InvalidArgumentException;
+use OneToMany\AI\Exception\RuntimeException;
+
+use function fopen;
+use function is_file;
+use function is_readable;
+use function sprintf;
+
+final readonly class CacheFileRequest implements CacheFileRequestInterface
+{
+    /**
+     * @param non-empty-lowercase-string $vendor
+     * @param non-empty-string $path
+     * @param non-negative-int $size
+     * @param non-empty-lowercase-string $format
+     * @param ?non-empty-lowercase-string $purpose
+     *
+     * @throws InvalidArgumentException the file does not exist or is not readable
+     */
+    public function __construct(
+        public string $vendor,
+        public string $path,
+        public string $name,
+        public int $size,
+        public string $format,
+        public ?string $purpose = null,
+    ) {
+        if (!is_file($this->path) || !is_readable($this->path)) {
+            throw new InvalidArgumentException(sprintf('The file "%s" does not exist or is not readable.', $path));
+        }
+    }
+
+    /**
+     * @see OneToMany\AI\Contract\Request\File\CacheFileRequestInterface
+     */
+    public function getVendor(): string
+    {
+        return $this->vendor;
+    }
+
+    /**
+     * @see OneToMany\AI\Contract\Request\File\CacheFileRequestInterface
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @see OneToMany\AI\Contract\Request\File\CacheFileRequestInterface
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @see OneToMany\AI\Contract\Request\File\CacheFileRequestInterface
+     */
+    public function getSize(): int
+    {
+        return $this->size;
+    }
+
+    /**
+     * @see OneToMany\AI\Contract\Request\File\CacheFileRequestInterface
+     */
+    public function getFormat(): string
+    {
+        return $this->format;
+    }
+
+    /**
+     * @see OneToMany\AI\Contract\Request\File\CacheFileRequestInterface
+     */
+    public function getPurpose(): ?string
+    {
+        return $this->purpose;
+    }
+
+    /**
+     * @see OneToMany\AI\Contract\Request\File\CacheFileRequestInterface
+     *
+     * @throws RuntimeException opening the file fails
+     */
+    public function open(): mixed
+    {
+        return @fopen($this->path, 'r') ?: throw new RuntimeException(sprintf('Opening the file "%s" failed.', $this->name));
+    }
+}
