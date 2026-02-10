c1ad562aa47b12a51361fbdd8943fa97e8110ee1
Vic Cherubini <vcherubini@gmail.com>
2026-02-10 15:46:15 -0600

    wip

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AI and LLM Library for PHP
 
-This library provides a single, unified, framework-independent library for integration with many popular AI platforms and large language models.
+This library provides a single, unified, framework-independent library for integration with several popular AI platforms and large language models.
 
 ## Installation
 
@@ -10,7 +10,57 @@ Install the library using Composer:
 composer require 1tomany/php-ai
 ```
 
+## Usage
+
+There are two ways to use this library:
+
+1. **Direct** Instantiate the AI client you wish to use and send a request object to it. This method is easier to use, but comes with the cost that your application will be less flexible and testable.
+2. **Actions** Register the clients you wish to use with a `OneToMany\AI\Factory\ClientFactory` instance, inject that instance into each action you wish to take, and interact with the action instead of through the client.
+
 **Note:** A [Symfony bundle](https://github.com/1tomany/php-ai-bundle) is available if you wish to integrate this library into your Symfony applications with autowiring and configuration support.
+
+I learn best by looking at actual code samples, so lets take a look at the two methods first.
+
+### Direct usage
+
+```php
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\PropertyInfo\Extractor\ConstructorExtractor;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+// Initialize the normalizers and serializer
+$constructorExtractor = new ConstructorExtractor(...[
+    'extractors' => [new PhpDocExtractor()]
+]);
+
+$typeExtractor = new PropertyInfoExtractor(...[
+    'typeExtractors' => [$constructorExtractor]
+]);
+
+$objectNormalizer = new ObjectNormalizer(...[
+    'propertyTypeExtractor' => $typeExtractor
+]);
+
+$this->serializer = new Serializer([
+    new BackedEnumNormalizer(),
+    new DateTimeNormalizer(),
+    new ArrayDenormalizer(),
+    $objectNormalizer,
+]);
+
+```
+
+
 
 ## Supported platforms
 
---
+++ examples/gemini/basic_query.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use OneToMany\AI\Client\Gemini\QueryClient;
+use OneToMany\AI\Request\Query\CompileRequest;
+use OneToMany\AI\Request\Query\ExecuteRequest;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\PropertyInfo\Extractor\ConstructorExtractor;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\UnwrappingDenormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+$apiKeyEnvVar = 'GEMINI_API_KEY';
+
+if (!$geminiApiKey = getenv($apiKeyEnvVar)) {
+    printf("Set the %s environment variable to continue.\n", $apiKeyEnvVar);
+    exit(1);
+}
+
+// Create a suitable Symfony Serializer
+$typeExtractor = new PropertyInfoExtractor([], [
+    new ConstructorExtractor([new PhpDocExtractor()]),
+]);
+
+$serializer = new Serializer([
+    new BackedEnumNormalizer(),
+    new DateTimeNormalizer(),
+    new ArrayDenormalizer(),
+    new UnwrappingDenormalizer(),
+    new ObjectNormalizer(propertyTypeExtractor: $typeExtractor),
+]);
+
+//
+$httpClient = HttpClient::create([
+    'timeout' => 120.0,
+]);
+
+// Create a client that will be used to compile and send queries
+$geminiQueryClient = new QueryClient($serializer, $httpClient, $geminiApiKey);
+
+// First, compile the query: this isn't strictly necessary, but by doing so,
+// you can log or otherwise modify the request that will be sent to the LLM.
+$compileRequest = new CompileRequest('gemini-2.5-flash')->withText(...[
+    'text' => 'Who invented the PHP programming language?',
+]);
+
+$response = $geminiQueryClient->compile($compileRequest);
+
+// Now that the query is compiled,
+// create a request to execute the query
+$executeRequest = new ExecuteRequest()
+    ->forModel($response->getModel())
+    ->withUrl($response->getUrl())
+    ->withRequest($response->getRequest());
+
+// Note: The `CompileResponse` object returned from
+// the `compile()` call has a handy method which does
+// the same work above but as a single method call:
+//
+//     $executeRequest = $response->toExecuteRequest();
+
+// Finally, execute the query with the model
+$response = $geminiQueryClient->execute($executeRequest);
+
+printf("The model '%s' generated the following output in %dms:\n\n%s\n", $response->getModel(), $response->getRuntime(), $response->getOutput());
