f1630babb5eb0dc6dff7080387aeedd92e4863f8
Vic Cherubini <vcherubini@gmail.com>
2026-02-13 00:29:36 -0600

    wip

--- examples/common/functions.php
+++ examples/common/functions.php
@@ -8,7 +8,7 @@
 function read_api_key(string $name): string
 {
     if (!$apiKey = getenv($name)) {
-        printf('Set the "%s" environment variable to continue.' . PHP_EOL, $name);
+        printf('Set the "%s" environment variable to continue.'.PHP_EOL, $name);
         exit(1);
     }
 
