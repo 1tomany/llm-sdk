6aa7862b113cf4a9bd79b78cfe02d7ee276c7ce2
Vic Cherubini <vcherubini@gmail.com>
2026-02-27 00:28:45 -0600

    wip

--- src/Client/Gemini/Type/Content/GenerateContentResponse.php
+++ src/Client/Gemini/Type/Content/GenerateContentResponse.php
@@ -5,6 +5,7 @@ namespace OneToMany\LlmSdk\Client\Gemini\Type\Content;
 use OneToMany\LlmSdk\Exception\RuntimeException;
 
 use function array_map;
+use function implode;
 use function sprintf;
 use function trim;
 
@@ -28,14 +29,8 @@ final readonly class GenerateContentResponse
      */
     public function getOutput(): string
     {
-        $output = null;
+        $output = array_map(fn ($c) => $c->getOutput(), $this->candidates);
 
-        if ([] !== $this->candidates) {
-            $output = array_map(fn ($c) => $c->getOutput(), $this->candidates);
-
-            $output = trim($this->candidates[0]->content->parts[0]->text) ?: null;
-        }
-
-        return $output ?? throw new RuntimeException(sprintf('The model "%s" failed to generate any output.', $this->modelVersion));
+        return trim(implode('', $output)) ?: throw new RuntimeException(sprintf('The model "%s" failed to generate any output.', $this->modelVersion));
     }
 }
