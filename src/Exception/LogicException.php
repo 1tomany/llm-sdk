00aa36f183c7d5544e3872d0b80b64e3696b7fd6
Vic Cherubini <vcherubini@gmail.com>
2026-03-05 16:47:14 -0600

    wip

--- src/Exception/LogicException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OneToMany\LlmSdk\Exception;
-
-use OneToMany\LlmSdk\Contract\Exception\ExceptionInterface;
-
-class LogicException extends \LogicException implements ExceptionInterface
-{
-}
