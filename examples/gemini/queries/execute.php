d08b7f1ef52354aa16d0df7deccf4861b8352835
Vic Cherubini <vcherubini@gmail.com>
2026-03-05 17:29:58 -0600

    wip

--- examples/gemini/queries/execute.php
+++ examples/gemini/queries/execute.php
@@ -18,14 +18,14 @@ $httpClient = HttpClient::create([
 ]);
 
 // Determine the Gemini model to use
-$geminiModel = read_model_name('gemini-2.5-flash');
+$model = read_model_name('gemini-2.5-flash');
 
 // Create a client that will be used to compile and send queries
 $queryClient = new QueryClient($serializer, $httpClient, $apiKey);
 
-// First, compile the query: this isn't strictly necessary, but by doing so,
-// you can log or otherwise modify the request that will be sent to the LLM.
-$compileRequest = new CompileRequest($geminiModel)->withPrompt(...[
+// First, compile the query: this is not strictly necessary,
+// but you can inspect the request that will be sent to the LLM
+$compileRequest = new CompileRequest($model)->withPrompt(...[
     'prompt' => 'Who invented the PHP programming language?',
 ]);
 
@@ -33,23 +33,18 @@ $response = $queryClient->compile(...[
     'request' => $compileRequest,
 ]);
 
-// Now that the query is compiled, create
-// a request to send it to the LLM server
-$executeRequest = new ExecuteRequest(...[
-    'model' => $geminiModel,
-]);
-
-$executeRequest->withUrl($response->getUrl())->withRequest(...[
-    'request' => $response->getRequest(),
-]);
+// Now that the query is compiled, create a request to send it to the LLM
+$executeRequest = new ExecuteRequest($model)->withUrl($response->getUrl())->withRequest($response->getRequest());
 
 // Note: The `CompileResponse` object returned from
-// the `compile()` call has a handy method which does
-// the same work above but as a single method call:
+// `compile()` has a handy method which does the same
+// work above but as a single method call:
 //
 //     $executeRequest = $response->toExecuteRequest();
 
-// Finally, execute the query with the model
-$response = $queryClient->execute($executeRequest);
+// Send the request to Gemini
+$response = $queryClient->execute(...[
+    'request' => $executeRequest,
+]);
 
 printf("The model '%s' generated the following output in %dms:\n\n%s\n", $response->getModel(), $response->getRuntime(), $response->getOutput());
