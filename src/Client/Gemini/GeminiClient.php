7cbc7d1569019313586904c5bc70b2a5b4ec5a62
Vic Cherubini <vcherubini@gmail.com>
2026-03-06 13:58:09 -0600

    wip

--- src/Client/Gemini/GeminiClient.php
+++ src/Client/Gemini/GeminiClient.php
@@ -2,26 +2,17 @@
 
 namespace OneToMany\LlmSdk\Client\Gemini;
 
-use OneToMany\LlmSdk\Contract\Client\ClientInterface;
+use OneToMany\LlmSdk\Client\BaseClient;
 use OneToMany\LlmSdk\Contract\Resource\BatchesResourceInterface;
 use OneToMany\LlmSdk\Contract\Resource\FilesResourceInterface;
 use OneToMany\LlmSdk\Contract\Resource\QueriesResourceInterface;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use OneToMany\LlmSdk\Resource\Gemini\BatchClient;
+use OneToMany\LlmSdk\Resource\Gemini\FileClient;
+use OneToMany\LlmSdk\Resource\Gemini\QueryClient;
 
-final class GeminiClient implements ClientInterface
+final class GeminiClient extends BaseClient
 {
-    private ?BatchesResourceInterface $batchesResource = null;
-    private ?FilesResourceInterface $filesResource = null;
-    private ?QueriesResourceInterface $queriesResource = null;
-
-    public function __construct(
-        private readonly DenormalizerInterface $denormalizer,
-        private readonly HttpClientInterface $httpClient,
-        #[\SensitiveParameter] private string $apiKey,
-        private readonly string $apiVersion = 'v1beta',
-    ) {
-    }
+    private string $apiVersion = 'v1beta';
 
     /**
      * @see OneToMany\LlmSdk\Contract\Client\ClientInterface
@@ -48,9 +39,9 @@ final class GeminiClient implements ClientInterface
      */
     public function batches(): BatchesResourceInterface
     {
-        $this->batchesResource ??= new BatchClient($this->denormalizer, $this->httpClient, $this->apiKey, $this->apiVersion);
+        $this->batches ??= new BatchClient($this->denormalizer, $this->httpClient, $this->apiKey, $this->apiVersion);
 
-        return $this->batchesResource;
+        return $this->batches;
     }
 
     /**
@@ -58,9 +49,9 @@ final class GeminiClient implements ClientInterface
      */
     public function files(): FilesResourceInterface
     {
-        $this->filesResource ??= new FileClient($this->denormalizer, $this->httpClient, $this->apiKey, $this->apiVersion);
+        $this->files ??= new FileClient($this->denormalizer, $this->httpClient, $this->apiKey, $this->apiVersion);
 
-        return $this->filesResource;
+        return $this->files;
     }
 
     /**
@@ -68,8 +59,8 @@ final class GeminiClient implements ClientInterface
      */
     public function queries(): QueriesResourceInterface
     {
-        $this->queriesResource ??= new QueryClient($this->denormalizer, $this->httpClient, $this->apiKey, $this->apiVersion);
+        $this->queries ??= new QueryClient($this->denormalizer, $this->httpClient, $this->apiKey, $this->apiVersion);
 
-        return $this->queriesResource;
+        return $this->queries;
     }
 }
