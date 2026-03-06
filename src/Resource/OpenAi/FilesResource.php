ab4d33d0d7957e492a50e935be5ff7b08c45c06d
Vic Cherubini <vcherubini@gmail.com>
2026-03-06 09:37:02 -0600

    wip

--- src/Resource/OpenAi/FilesResource.php
+++ src/Resource/OpenAi/FilesResource.php
@@ -2,7 +2,6 @@
 
 namespace OneToMany\LlmSdk\Resource\OpenAi;
 
-use OneToMany\LlmSdk\Resource\OpenAi\BaseClient;
 use OneToMany\LlmSdk\Client\OpenAi\Type\File\Enum\Purpose;
 use OneToMany\LlmSdk\Client\OpenAi\Type\File\File;
 use OneToMany\LlmSdk\Contract\Resource\FilesResourceInterface;
