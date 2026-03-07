49ab4fd209452da7814784a8865a992341960f13
Vic Cherubini <vcherubini@gmail.com>
2026-03-06 20:01:32 -0600

    wip

--- src/Response/Batch/BatchResponse.php
+++ src/Response/Batch/BatchResponse.php
@@ -4,8 +4,13 @@ namespace OneToMany\LlmSdk\Response\Batch;
 
 use OneToMany\LlmSdk\Response\BaseResponse;
 
-abstract readonly class BatchResponse extends BaseResponse
+readonly class BatchResponse extends BaseResponse
 {
+    /**
+     * @param non-empty-string $uri
+     * @param non-empty-string $status
+     * @param ?non-empty-string $fileUri
+     */
     public function __construct(
         string $model,
         private string $uri,
@@ -15,16 +20,25 @@ abstract readonly class BatchResponse extends BaseResponse
         parent::__construct($model);
     }
 
+    /**
+     * @return non-empty-string
+     */
     public function getUri(): string
     {
         return $this->uri;
     }
 
+    /**
+     * @return non-empty-string
+     */
     public function getStatus(): string
     {
         return $this->status;
     }
 
+    /**
+     * @return ?non-empty-string
+     */
     public function getFileUri(): ?string
     {
         return $this->fileUri;
