50f10de5d445cbdb0dc0200b8ef6dc3e2350605e
Vic Cherubini <vcherubini@gmail.com>
2026-03-13 14:24:13 -0500

    wip

--- src/Request/File/FileRequest.php
+++ src/Request/File/FileRequest.php
@@ -4,31 +4,20 @@ namespace OneToMany\LlmSdk\Request\File;
 
 use OneToMany\LlmSdk\Contract\Enum\Model;
 use OneToMany\LlmSdk\Contract\Enum\Vendor;
-use OneToMany\LlmSdk\Exception\InvalidArgumentException;
 
 class FileRequest
 {
-    private Vendor $vendor;
+    private readonly Vendor $vendor;
 
     public function __construct(
         string|Vendor|null $vendor = Vendor::Mock,
     ) {
-        $this->forVendor($vendor);
-    }
-
-    public static function fromModel(string|Model|null $model = Model::Mock): self
-    {
-        return new self(Model::create($model)->getVendor());
-    }
-
-    /**
-     * @throws InvalidArgumentException when the vendor does not exist
-     */
-    public function forVendor(string|Vendor|null $vendor): static
-    {
         $this->vendor = Vendor::create($vendor);
+    }
 
-        return $this;
+    public static function fromModel(string|Model|null $model = Model::Mock): static
+    {
+        return new static(Model::create($model)->getVendor());
     }
 
     public function getVendor(): Vendor
