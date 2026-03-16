3de127ff6601d7abf7608ee613d1f4ebc4bfab36
Vic Cherubini <vcherubini@gmail.com>
2026-03-16 11:47:27 -0500

    wip

--- src/Request/Embedding/CreateEmbeddingRequest.php
+++ src/Request/Embedding/CreateEmbeddingRequest.php
@@ -2,8 +2,26 @@
 
 namespace OneToMany\LlmSdk\Request\Embedding;
 
+use OneToMany\LlmSdk\Contract\Enum\Model;
 use OneToMany\LlmSdk\Request\BaseRequest;
 
 class CreateEmbeddingRequest extends BaseRequest
 {
+    /**
+     * @param array<string, mixed> $request
+     */
+    public function __construct(
+        string|Model|null $model,
+        private array $request,
+    ) {
+        parent::__construct($model);
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getRequest(): array
+    {
+        return $this->request;
+    }
 }
