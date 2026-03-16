1eac714570f19b380877d8e1724039ae1b53b8a8
Vic Cherubini <vcherubini@gmail.com>
2026-03-16 16:57:59 -0500

    wip

--- src/Resource/Mock/OutputsResource.php
+++ src/Resource/Mock/OutputsResource.php
@@ -8,6 +8,8 @@ use OneToMany\LlmSdk\Resource\Mock\Trait\GenerateIdTrait;
 use OneToMany\LlmSdk\Response\Output\GenerateOutputResponse;
 use OneToMany\LlmSdk\Response\Output\Usage\TokenUsage;
 
+use function assert;
+use function is_string;
 use function json_encode;
 use function random_int;
 use function strlen;
@@ -31,6 +33,12 @@ final readonly class OutputsResource implements OutputsResourceInterface
         /** @var non-empty-string $requestContent */
         $requestContent = json_encode($request->getRequest());
 
+        /**
+         * @var array{
+         *   id: non-empty-string,
+         *   text: non-empty-string,
+         * } $response
+         */
         $response = [
             'id' => $this->generateId('query'),
             'text' => $this->faker->sentence(),
@@ -39,9 +47,10 @@ final readonly class OutputsResource implements OutputsResourceInterface
         $output = $response['text'];
 
         if (isset($request->getRequest()['schema'])) {
-            /** @var non-empty-string $output */
             $output = json_encode(['output' => $output]);
         }
+
+        assert(is_string($output) && !empty($output));
 
         return new GenerateOutputResponse($request->getModel(), $response['id'], $response, $output, random_int(100, 10000), new TokenUsage(strlen($requestContent), 0, strlen($output)));
     }
