5ffe113ede3d8c52acb9ecd3555acba8b9cb4630
Vic Cherubini <vcherubini@gmail.com>
2026-03-16 11:52:50 -0500

    wip

---
+++ src/Response/Embedding/CreateEmbeddingResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+use OneToMany\LlmSdk\Contract\Enum\Model;
+use OneToMany\LlmSdk\Response\BaseResponse;
+
+final readonly class CreateEmbeddingResponse extends BaseResponse
+{
+    /**
+     * @param list<float> $embedding
+     */
+    public function __construct(
+        Model $model,
+        private array $embedding,
+        private int|float $runtime = 0,
+    ) {
+        parent::__construct($model);
+    }
+
+    /**
+     * @return list<float>
+     */
+    public function getEmbedding(): array
+    {
+        return $this->embedding;
+    }
+
+    /**
+     * @return non-negative-int
+     */
+    public function getRuntime(): int
+    {
+        return \max(0, (int) $this->runtime);
+    }
+}
