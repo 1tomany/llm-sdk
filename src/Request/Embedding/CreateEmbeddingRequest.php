61f61fa971a454534b4a1e1057ac46b8cbd94c16
Vic Cherubini <vcherubini@gmail.com>
2026-03-16 11:45:55 -0500

    wip

---
+++ src/Request/Embedding/CreateEmbeddingRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OneToMany\LlmSdk\Request\Embedding;
+
+use OneToMany\LlmSdk\Request\BaseRequest;
+
+class CreateEmbeddingRequest extends BaseRequest
+{
+}
---
+++ src/Request/Output/GenerateOutputRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OneToMany\LlmSdk\Request\Output;
+
+use OneToMany\LlmSdk\Contract\Enum\Model;
+use OneToMany\LlmSdk\Request\BaseRequest;
+
+class GenerateOutputRequest extends BaseRequest
+{
+    /**
+     * @param non-empty-string $url
+     * @param array<string, mixed> $request
+     */
+    public function __construct(
+        string|Model|null $model,
+        private readonly string $url,
+        private readonly array $request,
+    )
+    {
+        parent::__construct($model);
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getRequest(): array
+    {
+        return $this->request;
+    }
+}
