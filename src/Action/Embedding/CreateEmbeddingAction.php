1281eb6409b5586fc57c3faaf4059fdca8319b14
Vic Cherubini <vcherubini@gmail.com>
2026-03-17 13:59:24 -0500

    wip

--- src/Action/Embedding/CreateEmbeddingAction.php
+++ src/Action/Embedding/CreateEmbeddingAction.php
@@ -5,8 +5,10 @@ namespace OneToMany\LlmSdk\Action\Embedding;
 use OneToMany\LlmSdk\Action\BaseAction;
 use OneToMany\LlmSdk\Action\Trait\CompileQueryTrait;
 use OneToMany\LlmSdk\Contract\Action\Embedding\CreateEmbeddingActionInterface;
+use OneToMany\LlmSdk\Exception\InvalidArgumentException;
 use OneToMany\LlmSdk\Request\Embedding\CreateEmbeddingRequest;
 use OneToMany\LlmSdk\Request\Query\CompileQueryRequest;
+use OneToMany\LlmSdk\Request\Query\ProcessQueryRequest;
 use OneToMany\LlmSdk\Response\Embedding\CreateEmbeddingResponse;
 
 final readonly class CreateEmbeddingAction extends BaseAction implements CreateEmbeddingActionInterface
@@ -16,10 +18,14 @@ final readonly class CreateEmbeddingAction extends BaseAction implements CreateE
     /**
      * @see OneToMany\LlmSdk\Contract\Action\Embedding\CreateEmbeddingActionInterface
      */
-    public function act(CompileQueryRequest|CreateEmbeddingRequest $request): CreateEmbeddingResponse
+    public function act(CompileQueryRequest|ProcessQueryRequest $request): CreateEmbeddingResponse
     {
         if ($request instanceof CompileQueryRequest) {
             $request = $this->compileQuery($request)->toCreateEmbeddingRequest();
+        }
+
+        if (!$request->getModel()->isEmbedding()) {
+            throw new InvalidArgumentException(\sprintf('Creating the embedding failed because the model "%s" is not an embedding model.', $request->getModel()->getValue()));
         }
 
         return $this->createClient($request->getVendor())->embeddings()->create($request);
--- src/Contract/Action/Embedding/CreateEmbeddingActionInterface.php
+++ src/Contract/Action/Embedding/CreateEmbeddingActionInterface.php
@@ -4,9 +4,10 @@ namespace OneToMany\LlmSdk\Contract\Action\Embedding;
 
 use OneToMany\LlmSdk\Request\Embedding\CreateEmbeddingRequest;
 use OneToMany\LlmSdk\Request\Query\CompileQueryRequest;
+use OneToMany\LlmSdk\Request\Query\ProcessQueryRequest;
 use OneToMany\LlmSdk\Response\Embedding\CreateEmbeddingResponse;
 
 interface CreateEmbeddingActionInterface
 {
-    public function act(CompileQueryRequest|CreateEmbeddingRequest $request): CreateEmbeddingResponse;
+    public function act(CompileQueryRequest|ProcessQueryRequest $request): CreateEmbeddingResponse;
 }
