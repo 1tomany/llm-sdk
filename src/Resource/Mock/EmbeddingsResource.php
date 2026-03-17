7c964a4852009459d3998e142c720b9818b4ead1
Vic Cherubini <vcherubini@gmail.com>
2026-03-17 10:42:42 -0500

    wip

--- src/Resource/Mock/EmbeddingsResource.php
+++ src/Resource/Mock/EmbeddingsResource.php
@@ -5,10 +5,13 @@ namespace OneToMany\LlmSdk\Resource\Mock;
 use OneToMany\LlmSdk\Contract\Resource\EmbeddingsResourceInterface;
 use OneToMany\LlmSdk\Request\Embedding\CreateEmbeddingRequest;
 use OneToMany\LlmSdk\Response\Embedding\CreateEmbeddingResponse;
+use OneToMany\LlmSdk\Response\Usage\TokenUsage;
 
 use function assert;
 use function count;
+use function json_encode;
 use function random_int;
+use function strlen;
 
 final readonly class EmbeddingsResource extends BaseResource implements EmbeddingsResourceInterface
 {
@@ -17,6 +20,12 @@ final readonly class EmbeddingsResource extends BaseResource implements Embeddin
      */
     public function create(CreateEmbeddingRequest $request): CreateEmbeddingResponse
     {
+        /** @var non-empty-string $requestContent */
+        $requestContent = json_encode($request->getRequest());
+
+        /**
+         * @var non-empty-list<float>
+         */
         $embedding = [];
 
         /** @var positive-int $embeddingDimensions */
@@ -28,6 +37,6 @@ final readonly class EmbeddingsResource extends BaseResource implements Embeddin
 
         assert(count($embedding) === $embeddingDimensions);
 
-        return new CreateEmbeddingResponse($request->getModel(), $embedding, random_int(100, 10000));
+        return new CreateEmbeddingResponse($request->getModel(), $embedding, random_int(100, 10000), new TokenUsage(strlen($requestContent)));
     }
 }
