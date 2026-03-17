c8684c432677b93d30b56543b73354b1d798cbf6
Vic Cherubini <vcherubini@gmail.com>
2026-03-16 20:59:55 -0500

    wip

--- src/Resource/Gemini/Type/Batch/InputConfig.php
+++ src/Resource/Gemini/Type/Batch/InputConfig.php
@@ -7,7 +7,8 @@ final readonly class InputConfig
     /**
      * @param non-empty-string $fileName
      */
-    public function __construct(public string $fileName)
-    {
+    public function __construct(
+        public string $fileName,
+    ) {
     }
 }
