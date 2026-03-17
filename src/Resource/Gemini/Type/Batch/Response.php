6dee2e9007d1475328013d0c2df22f5fc863f231
Vic Cherubini <vcherubini@gmail.com>
2026-03-16 21:00:36 -0500

    wip

--- src/Resource/Gemini/Type/Batch/Response.php
+++ src/Resource/Gemini/Type/Batch/Response.php
@@ -7,7 +7,8 @@ final readonly class Response
     /**
      * @param non-empty-string $responsesFile
      */
-    public function __construct(public string $responsesFile)
-    {
+    public function __construct(
+        public string $responsesFile,
+    ) {
     }
 }
