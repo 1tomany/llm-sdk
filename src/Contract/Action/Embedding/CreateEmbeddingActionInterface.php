4fc7f59918be431b0d6e13b002369bd2f72d0a98
Vic Cherubini <vcherubini@gmail.com>
2026-03-17 10:51:58 -0500

    wip

--- src/Contract/Action/Embedding/CreateEmbeddingActionInterface.php
+++ src/Contract/Action/Embedding/CreateEmbeddingActionInterface.php
@@ -2,7 +2,6 @@
 
 namespace OneToMany\LlmSdk\Contract\Action\Embedding;
 
-use OneToMany\LlmSdk\Exception\InvalidArgumentException;
 use OneToMany\LlmSdk\Request\Embedding\CreateEmbeddingRequest;
 use OneToMany\LlmSdk\Request\Query\CompileQueryRequest;
 use OneToMany\LlmSdk\Response\Embedding\CreateEmbeddingResponse;
