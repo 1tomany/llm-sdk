bc9d70fba8cda83c248f619aebcc5d7a38693715
Vic Cherubini <vcherubini@gmail.com>
2026-03-17 22:02:23 -0500

    wip

---
+++ tests/Request/Batch/CreateBatchRequestTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OneToMany\LlmSdk\Tests\Request\Batch;
+
+use OneToMany\LlmSdk\Contract\Enum\Model;
+use OneToMany\LlmSdk\Exception\InvalidArgumentException;
+use OneToMany\LlmSdk\Request\Batch\CreateBatchRequest;
+use PHPUnit\Framework\TestCase;
+
+final class CreateBatchRequestTest extends TestCase
+{
+    public function testConstructorRequiresNonEmptyTrimmedName(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The name cannot be empty.');
+
+        new CreateBatchRequest(Model::Mock, '  ');
+    }
+}
