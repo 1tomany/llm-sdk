6d7dbae6624c8c0220b2f357a1ba799dcd0326d6
Vic Cherubini <vcherubini@gmail.com>
2026-03-17 21:44:52 -0500

    wip

---
+++ tests/Action/Embedding/CreateEmbeddingActionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace OneToMany\LlmSdk\Tests\Action\Embedding;
+
+use OneToMany\LlmSdk\Action\Embedding\CreateEmbeddingAction;
+use OneToMany\LlmSdk\Contract\Enum\Model;
+use OneToMany\LlmSdk\Exception\InvalidArgumentException;
+use OneToMany\LlmSdk\Factory\ClientContainer;
+use OneToMany\LlmSdk\Factory\ClientFactory;
+use OneToMany\LlmSdk\Request\Query\CompileQueryRequest;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+
+#[Group('UnitTests')]
+#[Group('ActionTests')]
+#[Group('EmbeddingTests')]
+final class CreateEmbeddingActionTest extends TestCase
+{
+    public function testCreatingEmbeddingRequiresEmbeddingModel(): void
+    {
+        $model = Model::Mock;
+        $this->assertFalse($model->isEmbedding());
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The model "'.$model->getValue().'" is not an embedding model.');
+
+        new CreateEmbeddingAction(new ClientFactory(new ClientContainer()))->act(new CompileQueryRequest($model));
+    }
+}
