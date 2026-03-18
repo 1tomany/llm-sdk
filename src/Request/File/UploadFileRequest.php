73fb78120528564437db1aa5a6f7fe25576a2594
Vic Cherubini <vcherubini@gmail.com>
2026-03-17 22:12:05 -0500

    wip

--- src/Request/File/UploadFileRequest.php
+++ src/Request/File/UploadFileRequest.php
@@ -85,7 +85,11 @@ class UploadFileRequest
     public function getSize(): int
     {
         if (null === $this->size) {
-            $this->size = @filesize($this->getPath()) ?: throw new RuntimeException(sprintf('Calculating the size of the file "%s" failed.', $this->getName()));
+            if (false === $size = @filesize($this->getPath())) {
+                throw new RuntimeException(sprintf('Calculating the size of the file "%s" failed.', $this->getName()));
+            }
+
+            $this->size = $size;
         }
 
         return $this->size;
