ecc7e0ff13ba39f151d41d898ea29e2894638f60
Vic Cherubini <vcherubini@gmail.com>
2026-04-04 01:59:49 -0500

    wip

--- src/Resource/Gemini/Type/Response/FileSearchStore/FileSearchStore.php
+++ src/Resource/Gemini/Type/Response/FileSearchStore/FileSearchStore.php
@@ -9,20 +9,20 @@ final readonly class FileSearchStore
     /**
      * @param non-empty-string $name
      * @param non-empty-string $displayName
-     * @param ?numeric-string $activeDocumentsCount
-     * @param ?numeric-string $pendingDocumentsCount
-     * @param ?numeric-string $failedDocumentsCount
-     * @param ?numeric-string $sizeBytes
+     * @param numeric-string $activeDocumentsCount
+     * @param numeric-string $pendingDocumentsCount
+     * @param numeric-string $failedDocumentsCount
+     * @param numeric-string $sizeBytes
      */
     public function __construct(
         public string $name,
         public string $displayName,
         public \DateTimeImmutable $createTime,
         public \DateTimeImmutable $updateTime,
-        public ?string $activeDocumentsCount = null,
-        public ?string $pendingDocumentsCount = null,
-        public ?string $failedDocumentsCount = null,
-        public ?string $sizeBytes = null,
+        public string $activeDocumentsCount = '0',
+        public string $pendingDocumentsCount = '0',
+        public string $failedDocumentsCount = '0',
+        public string $sizeBytes = '0',
     ) {
     }
 
