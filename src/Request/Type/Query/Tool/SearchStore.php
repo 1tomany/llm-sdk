fc27443fbb8c36cf5f8ac1f920d3ca9654cf1e54
Vic Cherubini <vcherubini@gmail.com>
2026-04-07 10:58:47 -0500

    wip

--- src/Request/Type/Query/Tool/SearchStore.php
+++ src/Request/Type/Query/Tool/SearchStore.php
@@ -2,7 +2,9 @@
 
 namespace OneToMany\LlmSdk\Request\Type\Query\Tool;
 
-final readonly class SearchStore
+use OneToMany\LlmSdk\Contract\Request\Type\Query\Tool\ToolInterface;
+
+final readonly class SearchStore implements ToolInterface
 {
     /**
      * @param non-empty-string $uri
